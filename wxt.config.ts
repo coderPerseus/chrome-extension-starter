b62082e74a985f78eb41308953819e7107425f47
luckySnail <snailrun160@gmail.com>
2026-01-24 17:27:31 +0800

    feat: 集成 @wxt-dev/auto-icons

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,9 +1,13 @@
-import { defineConfig } from "wxt"
+import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ["@wxt-dev/module-react"],
+  modules: ["@wxt-dev/module-react", "@wxt-dev/auto-icons"],
+  autoIcons: {
+    baseIconPath: "assets/logo.png",
+    developmentIndicator: false,
+  },
   manifest: {
     permissions: ["storage"],
   },
-})
+});
